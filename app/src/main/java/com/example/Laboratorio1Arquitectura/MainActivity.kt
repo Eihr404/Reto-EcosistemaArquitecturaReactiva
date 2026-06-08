e0c9e405350f12d4d4657221072d14d215719d66
Eihr404 <israhernandezr@gmail.com>
2026-06-07 19:44:51 -0500

    feat: Creacion del MainActivity

--- app/src/main/java/com/example/Laboratorio1Arquitectura/MainActivity.kt
+++ app/src/main/java/com/example/Laboratorio1Arquitectura/MainActivity.kt
@@ -4,28 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.Laboratorio1Arquitectura.data.repository.InMemoryTaskRepository
-import com.example.Laboratorio1Arquitectura.domain.usecase.AddTaskUseCase
-import com.example.Laboratorio1Arquitectura.domain.usecase.GetTasksUseCase
-import com.example.Laboratorio1Arquitectura.ui.presentation.tasks.AcademicTaskApp
-import com.example.Laboratorio1Arquitectura.ui.presentation.tasks.AcademicTaskViewModel
+import androidx.activity.viewModels
+import androidx.compose.material3.MaterialTheme
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.Laboratorio1Arquitectura.data.repository.InMemoryGradeRepository
+import com.example.Laboratorio1Arquitectura.domain.usecase.AddGradeUseCase
+import com.example.Laboratorio1Arquitectura.domain.usecase.GetGradesUseCase
+import com.example.Laboratorio1Arquitectura.ui.presentation.grades.GradeTrackerApp
+import com.example.Laboratorio1Arquitectura.ui.presentation.grades.GradeViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val gradeViewModel: GradeViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val repository = InMemoryGradeRepository()
+
+                val getGradesUseCase = GetGradesUseCase(repository)
+                val addGradeUseCase = AddGradeUseCase(repository)
+
+                return GradeViewModel(
+                    getGradesUseCase = getGradesUseCase,
+                    addGradeUseCase = addGradeUseCase
+                ) as T
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
-        // orquestación de dependencias de infraestructura y negocio
-        val repository = InMemoryTaskRepository()
-        val getTasksUseCase = GetTasksUseCase(repository)
-        val addTaskUseCase = AddTaskUseCase(repository)
-
-        // creación del ViewModel inyectando sus colaboradores requeridos
-        val viewModel = AcademicTaskViewModel(getTasksUseCase, addTaskUseCase, repository)
-
-        // vista declarativa limpia
         setContent {
-            AcademicTaskApp(viewModel = viewModel)
+            MaterialTheme {
+                GradeTrackerApp(
+                    viewModel = gradeViewModel
+                )
+            }
         }
     }
 }
